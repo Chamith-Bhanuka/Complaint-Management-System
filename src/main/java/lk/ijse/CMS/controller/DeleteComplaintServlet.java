d180f516091e5e4d1260d7d80d3d3c295aadd96b
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-06-20 15:32:58 +0530

    create delete complaint servlet

---
+++ src/main/java/lk/ijse/CMS/controller/DeleteComplaintServlet.java
@@ -0,0 +1,33 @@
+package lk.ijse.CMS.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lk.ijse.CMS.dao.ComplaintDAO;
+import lk.ijse.CMS.model.Complaint;
+import lk.ijse.CMS.model.User;
+
+import java.io.IOException;
+
+public class DeleteComplaintServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null || !"employee".equals(user.getRole())) {
+            res.sendRedirect("jsp/login.jsp");
+            return;
+        }
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        Complaint complaint = ComplaintDAO.getComplaintById(id);
+
+        if (complaint != null && complaint.getUserId() == user.getId()
+                && "Pending".equalsIgnoreCase(complaint.getStatus())) {
+            ComplaintDAO.deleteComplaint(id);
+        }
+
+        res.sendRedirect("jsp/view_complaints.jsp?deleted=true");
+    }
+}
