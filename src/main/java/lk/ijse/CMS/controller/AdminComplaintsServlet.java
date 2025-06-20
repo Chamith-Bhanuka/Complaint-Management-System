d70625871bf2ab8a319edbc98474494c4fa3dc6d
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-06-20 15:50:59 +0530

    create admin complaints servlet

---
+++ src/main/java/lk/ijse/CMS/controller/AdminComplaintsServlet.java
@@ -0,0 +1,31 @@
+package lk.ijse.CMS.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lk.ijse.CMS.dao.ComplaintDAO;
+import lk.ijse.CMS.model.Complaint;
+import lk.ijse.CMS.model.User;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/complaints")
+public class AdminComplaintsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null || !"admin".equalsIgnoreCase(user.getRole())) {
+            res.sendRedirect("../jsp/login.jsp");
+            return;
+        }
+
+        List<Complaint> complaints = ComplaintDAO.getAllComplaints();
+        req.setAttribute("complaints", complaints);
+        req.getRequestDispatcher("/jsp/admin_complaints.jsp").forward(req, res);
+    }
+}
